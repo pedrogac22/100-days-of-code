f649eb3e2729c865d82d317a451a2efce1b6e64c
Pedro Gomes <pedrogac2@gmail.com>
2025-04-21 21:29:59 -0300

    first day: add string, variables, input and print example

---
+++ day_1/challenge.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator.\n")
+print("")
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("")
+print("Your band name could be " + city_name + " " + pet_name )
---
+++ day_1/main.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("Hello" + " " + "Pedro")
+
+print("Hello " + input("What is your name? \n") + "!")
+
+age = input("how old are you?\n")
+print(type(age))
